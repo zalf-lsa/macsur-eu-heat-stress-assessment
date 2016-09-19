8de835f10fc1c8d6f20d5ca4202e10a68eea5124
Michael Berg <michael.berg@zalf.de>
2016-09-19 10:57:19 +0200

    initial commit

---
+++ run-work-collector.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+# -*- coding: ISO-8859-15-*-
+
+# pragma pylint: disable=mixed-indentation
+
+import zmq
+#print zmq.pyzmq_version()
+import time
+import pprint
+import types
+import json
+import csv
+import sys
+sys.path.append("C:/Users/berg.ZALF-AD/GitHub/monica/project-files/Win32/Release")
+import monica_python
+
+OP_AVG = 0
+OP_MEDIAN = 1
+OP_SUM = 2
+OP_MIN = 3
+OP_MAX = 4
+OP_FIRST = 5
+OP_LAST = 6
+OP_NONE = 7
+OP_UNDEFINED_OP_ = 8
+
+ORGAN_ROOT = 0
+ORGAN_LEAF = 1
+ORGAN_SHOOT = 2
+ORGAN_FRUIT = 3
+ORGAN_STRUCT = 4
+ORGAN_SUGAR = 5
+ORGAN_UNDEFINED_ORGAN_ = 6
+
+def oidIsOrgan(oid):
+	return oid["organ"] != ORGAN_UNDEFINED_ORGAN_
+
+def oidIsRange(oid):
+	return oid["fromLayer"] >= 0 \
+		and oid["toLayer"] >= 0 \
+		and oid["fromLayer"] < oid["toLayer"] 
+
+def opToString(op):
+	return { 
+		  OP_AVG: "AVG"
+		, OP_MEDIAN: "MEDIAN"
+		, OP_SUM: "SUM"
+		, OP_MIN: "MIN"
+		, OP_MAX: "MAX"
+		, OP_FIRST: "FIRST"
+		, OP_LAST: "LAST"
+		, OP_NONE: "NONE"
+		, OP_UNDEFINED_OP_: "undef"
+		}.get(op, "undef")
+
+def organToString(organ):
+	return {
+		  ORGAN_ROOT: "Root"
+		, ORGAN_LEAF: "Leaf"
+		, ORGAN_SHOOT: "Shoot"
+		, ORGAN_FRUIT: "Fruit"
+		, ORGAN_STRUCT: "Struct"
+		, ORGAN_SUGAR: "Sugar"
+		, ORGAN_UNDEFINED_ORGAN_: "undef"
+		}.get(organ, "undef")
+
+def oidToString(oid, includeTimeAgg):
+	oss = ""
+	oss += "["
+	oss += oid["name"]
+	if oidIsOrgan(oid):
+		oss += ", " + organToString(oid["organ"])
+	elif oidIsRange(oid):
+		oss += ", [" + str(oid["fromLayer"] + 1) + ", " + str(oid["toLayer"] + 1) \
+		+ (", " + opToString(oid["layerAggOp"]) if oid["layerAggOp"] != OP_NONE else "") \
+		+ "]"
+	elif oid["fromLayer"] >= 0:
+		oss += ", " + str(oid["fromLayer"] + 1)
+	if includeTimeAgg:
+		oss += ", " + opToString(oid["timeAggOp"])
+	oss += "]"
+
+	return oss
+
+def writeOutputHeaderRows(outputIds, csvSep, includeHeaderRow, includeUnitsRow, includeTimeAgg):
+	row1 = []
+	row2 = []
+	row3 = []
+	row4 = []
+	for oid in outputIds:
+		fromLayer = oid["fromLayer"]
+		toLayer = oid["toLayer"]
+		isOrgan = oidIsOrgan(oid)
+		isRange = oidIsRange(oid) and oid["layerAggOp"] == OP_NONE
+		if isOrgan:
+			toLayer = fromLayer = oid["organ"] # organ is being represented just by the value of fromLayer currently
+		elif isRange:
+			fromLayer += 1
+			toLayer += 1     # display 1-indexed layer numbers to users
+		else:
+			toLayer = fromLayer # for aggregated ranges, which aren't being displayed as range
+
+		for i in range(fromLayer, toLayer+1):
+			str1 = ""
+			if isOrgan:
+				str1 += oid["name"] + "/" + organToString(oid["organ"])
+			elif isRange:
+				str1 += oid["name"] + "_" + str(i)
+			else:
+				str1 += oid["name"]
+			row1.append(str1)
+			row4.append("j:" + oid["jsonInput"].replace("\"", ""))
+			row3.append("m:" + oidToString(oid, includeTimeAgg))
+			row2.append("[" + oid["unit"] + "]")
+
+	out = []
+	if includeHeaderRow:
+		out.append(row1)
+		out.append(row4)
+		out.append(row3)
+		out.append(row2)
+	
+	return out
+
+
+def writeOutput(outputIds, values, csvSep):
+	out = []
+	if len(values) > 0:
+		for k in range(0, len(values[0])):
+			i = 0
+			row = []
+			for oid in outputIds:
+				j = values[i][k]
+				if type(j) == types.ListType:
+					for jv in j:
+						row.append(jv)
+				else:
+					row.append(j)
+				i += 1
+			out.append(row)
+	return out
+
+
+csvSep = ","
+includeHeaderRow = True
+includeUnitsRow = True
+
+
+def parseOutputIds(oids):
+		j = json.dumps(oids)
+		rs = monica_python.parseOutputIdsToJsonString(j)
+		p = json.loads(rs, "latin-1")
+		return p
+
+
+def collector():
+	context = zmq.Context()
+	socket = context.socket(zmq.PULL)
+	socket.bind("tcp://*:7777")
+	collector_data = {}
+	leave = False
+	while not leave:
+		result = socket.recv_json()
+		if result["type"] == "finish":
+			print "received finish message"
+			leave = True
+		else:
+			print "received work results"
+			d = result["daily"]
+			
+			outputIds = parseOutputIds(["Date", "Crop", "Stage", "Yield", "SumYield", "Tmin", "Tavg", "Tmax", "Wind", "Globrad", "Relhumid", "Sunhours"])
+			
+			hout = writeOutputHeaderRows(outputIds, ",", True, True, True)
+			rout = writeOutput(outputIds, d, ",")
+			
+			with open("out.csv", 'wb') as f:
+				writer = csv.writer(f)
+				for row in hout:
+					writer.writerow(row)
+				for row in rout:
+					writer.writerow(row)
+
+			#c = result["crop"]
+			#print c
+			#for cropName, data in c.iteritems(): 
+			#	collector_data[cropName] = collector_data.setdefault(cropName, 0) + data[2]
+		#print "intermediate results: ", collector_data
+
+	#print "final results: ", collector_data
+
+collector()
+
---
+++ run-work-producer.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+# -*- coding: ISO-8859-15-*-
+
+# pragma pylint: disable=mixed-indentation
+
+import time
+import zmq
+import json
+import csv
+from StringIO import StringIO
+from datetime import date, datetime, timedelta
+#import types
+import sys
+sys.path.append("C:/Users/berg.ZALF-AD/GitHub/monica/project-files/Win32/Release")	 # path to monica_python.pyd or monica_python.so
+#sys.path.append('C:/Users/berg.ZALF-AD/GitHub/util/soil')
+#from soil_conversion import *
+#import monica_python
+from env_json_from_json_config import createEnvJsonFromJsonConfig
+
+#print "pyzmq version: ", zmq.pyzmq_version()
+#print "sys.path: ", sys.path
+#print "sys.version: ", sys.version
+
+def main():
+	with open("sim.json") as f:
+		sim = json.load(f)
+
+	with open("site.json") as f:
+		site = json.load(f)
+
+	with open("crop.json") as f:
+		crop = json.load(f)
+
+	sim["include-file-base-path"] = "C:/Users/berg.ZALF-AD/MONICA"
+	#sim["climate.csv"] = "35_120_v1.csv"
+	sim["climate.csv"] = "C:/Users/berg.ZALF-AD/MONICA/Examples/Hohenfinow2/climate.csv"
+
+	wwPheno = {}
+	with open("WW_pheno.csv") as f:
+		reader = csv.reader(f)
+		reader.next()
+		for row in reader:
+			wwPheno[(int(row[0]), int(row[1]))] = {
+				  "sowing-doy": int(row[6]) 
+				, "flowering-doy": int(row[7]) 
+				, "harvest-doy": int(row[8])
+				}
+
+	soil = {}
+	with open("JRC_soil_macsur.csv") as f:
+		reader = csv.reader(f)
+		reader.next()
+		for row in reader:
+			soil[(int(row[1]), int(row[0]))] = { 
+				  "elevation": float(row[4])
+				, "latitude": float(row[5])
+				, "depth": float(row[6])
+				, "pwp": float(row[7])
+				, "fc": float(row[8])
+				, "sat": float(row[9])
+				, "sw-init": float(row[10])
+				, "oc-topsoil": float(row[11])
+				, "oc-subsoil": float(row[12])
+				, "bd-topsoil": float(row[13])
+				, "bd-subsoil": float(row[14])
+				}
+
+	row = 48
+	col = 42
+	s = soil[(row, col)]
+	p = wwPheno[(row, col)]
+	
+	startDate = date(1980, 1, 1)# + timedelta(days = p["sowing-doy"])
+	sim["start-date"] = startDate.isoformat()
+	sim["end-date"] = date(2010, 12, 31).isoformat()
+	sim["debug?"] = True
+
+	seedingDate = date(1980, 1, 1) + timedelta(days = p["sowing-doy"])
+	crop["cropRotation"][0]["worksteps"][0]["date"] = seedingDate.strftime("0000-%m-%d")
+	harvestDate = date(1980, 1, 1) + timedelta(days = p["harvest-doy"])
+	crop["cropRotation"][0]["worksteps"][1]["date"] = harvestDate.strftime("0000-%m-%d")
+
+	site["Latitude"] = s["latitude"]
+	pwp = s["pwp"]
+	fc = s["fc"]
+	smPercentFC = (((s["sw-init"] - pwp) / 0.7) + pwp) / fc * 100.0
+	top = { 
+		  "Thickness": [0.3, "m"]
+		, "SoilOrganicCarbon": [s["oc-topsoil"], "%"]
+		, "SoilBulkDensity": [s["bd-topsoil"] * 1000, "kg m-3"]
+		, "FieldCapacity": [fc, "m3 m-3"]
+		, "PermanentWiltingPoint": [pwp, "m3 m-3"]
+		, "PoreVolume": [s["sat"], "m3 m-3"]
+		, "SoilMoisturePercentFC": [smPercentFC, "% [0-100]"]
+		, "Lambda": 0.5
+		}
+	sub = {
+		  "Thickness": [1.7, "m"] 
+		, "SoilOrganicCarbon": [s["oc-subsoil"], "%"]
+		, "SoilBulkDensity": [s["bd-subsoil"] * 1000, "kg m-3"]
+		, "FieldCapacity": [fc, "m3 m-3"]
+		, "PermanentWiltingPoint": [pwp, "m3 m-3"]
+		, "PoreVolume": [s["sat"], "m3 m-3"]
+		, "SoilMoisturePercentFC": [smPercentFC, "% [0-100]"]
+		, "Lambda": 0.5
+		} 
+
+	site["SiteParameters"]["SoilProfileParameters"] = [top, sub] 
+	#print site["SiteParameters"]["SoilProfileParameters"]
+
+	period = "0"
+	gcm_rcp = "0_0" #"GFDL-CM3_45"
+	
+	def csv2string(data):
+		si = StringIO()
+		cw = csv.writer(si)
+		cw.writerow(data)
+		return si.getvalue().strip('\r\n')
+
+	climateCSVString = ""
+	lastInSection = False
+	with open("35_120_v1.csv") as f:
+		reader = csv.reader(f)
+		reader.next()
+		climateCSVString += csv2string(["iso-date", "tmin", "tavg", "tmax", "precip", "globrad", "wind", "relhumid"]) + "\n"
+		for row in reader:
+			line = [
+				  datetime.strptime(row[2], "%Y%m%d").date().isoformat()
+				, float(row[4])
+				, (float(row[3]) + float(row[4])) / 2
+				, float(row[3])
+				, float(row[7])
+				, float(row[8])
+				, float(row[6]) / 24 / 3.6
+				, (float(row[9]) + float(row[10])) / 2
+				]
+
+			currentlyInSection = row[0] == period and row[1] == gcm_rcp
+			if lastInSection and not currentlyInSection:
+				break
+			else: 
+				if currentlyInSection:
+					climateCSVString += csv2string(line) + "\n" #str(line)[1:-1] + "\n"
+			lastInSection = currentlyInSection
+
+	env = createEnvJsonFromJsonConfig({
+		  "crop": crop
+		, "site": site
+		, "sim": sim
+		, "climate": climateCSVString
+		})
+	#print env
+
+	producer(env)
+
+
+def producer(env):
+	context = zmq.Context()
+	socket = context.socket(zmq.PUSH)
+	socket.bind("tcp://*:6666")
+
+	#consumerSocket = context.socket(zmq.PUSH)
+	#consumerSocket.connect("tcp://localhost:7777");
+	
+	#workerControlSocket = context.socket(zmq.PUB)
+	#consumerSocket.bind("tcp://*:6666");
+
+	#time.sleep(2)
+
+	#for i in range(1):
+	socket.send_json(env)
+
+	# Start your result manager and workers before you start your producers
+	
+	#time.sleep(10)
+	#workerControlSocket.send("finish " + json.dumps({"type": "finish"}))
+	#consumerSocket.send_json({"type": "finish"})
+
+
+
+main()
